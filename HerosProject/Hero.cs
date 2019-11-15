8c7883201b8f9ffcf527b57e5a89dd96aa324a51
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2019-11-15 09:15:22 +0200

    Add files via upload

---
+++ HerosProject/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerosProject
+{
+    class Hero
+    {
+        private String _name;
+        private String _locale;
+        public Hero(String name, String locale)
+        {
+            _name = name;
+            _locale = locale;
+        }
+
+        public override string ToString()
+        {
+            return "Name\t\t: "+_name+"\nLocale\t\t: "+_locale;
+        }
+        public string Name { get => _name; set => _name = value; }
+        public string Locale { get => _locale; set => _locale = value; }
+
+        public void Save (int DangeredPeople)
+        {
+            return;
+        }
+    }
+}
---
+++ HerosProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerosProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero k1 = new Hero("Batman*", "London");
+            k1.Name = ("Batman*");
+            Console.WriteLine("Hero number one is: " + k1.Name);
+            Hero k2 = new Hero("Superman", "Tokyo");
+            Hero k3 = new Hero("Kyogo*", "Peking");
+            Console.ReadLine();
+        }
+    }
+}
